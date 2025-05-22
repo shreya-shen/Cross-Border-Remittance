6094a59b4ff56a51f3f1cfc94c76fdff4bb877b3
zGrish <grishmglr0@gmail.com>
2025-05-22 06:34:05 +0530

    .

--- RemittanceApp/services/api.js
+++
@@ -1,3 +0,0 @@
-axios.post('/api/kyc', formData)
-  .then(res => console.log(res))
-  .catch(err => console.error(err));
--- RemittanceApp/services/web3.js
+++
@@ -1,6 +0,0 @@
-import Web3 from 'web3';
-
-const web3 = new Web3(window.ethereum);
-await window.ethereum.request({ method: 'eth_requestAccounts' });
-const accounts = await web3.eth.getAccounts();
-console.log("Connected account:", accounts[0]);
